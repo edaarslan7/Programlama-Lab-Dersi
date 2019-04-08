8e78d84fde7578680c48adc117d7fe51d18d5942
Eda Arslan <34687342+edaarslan7@users.noreply.github.com>
2019-04-08 20:24:18 +0300

    Add files via upload

---
+++ Soru6.py
@@ -0,0 +1,56 @@
+#-------------soru 6:lab uygulamalarında kelime frekanslarının oldugu fonksiyonu hash yapısını listeye aktaran fonksiyon
+#b)tersini yani iki boyutlu listeyi hash yapısına altaran fonk------------------
+#m*n boyutlarında btün elemanları -1 olan 2 boyutlu bir liste oluştur
+import random
+
+def myFunctionCreate(m: int,n: int):
+    myList=[]
+    for i in range(m):
+        myList.append([])
+        for j in range(n):
+            #s=random.randint(-1,1)
+            s=-1
+            myList[i].append(s)
+    return myList
+
+def myFunctionPrint(myList):
+    m=len(myList)
+    n=len(myList[0])
+    for i in range(m):
+        for j in range(n):
+            print(myList[i][j],end=" ")
+        print( )
+
+#my2ndArray=myFunctionCreate(3,2)
+#myFunctionPrint(my2ndArray)
+
+def myFunctionConvertToHash(myList):
+    myDict={}
+    m=len(myList)
+    n=len(myList[0])
+    for i in range(m):
+        for j in range(n):
+            myDict[(i,j)]=myList[i][j]
+            #print(myList[i][j],end=" ")
+        #print( )
+    return myDict
+
+def myFunctionPrintHash(myHashList):
+    print("  ")
+    for key in myHashList:
+        print(myHashList[key],end=" ")
+
+
+#myList=(myFunctionCreate(3,2))
+#myFunctionPrintHash(myFunctionConvertToHash(myList))
+
+def myFunctionHashToList(myHashList):
+    myList=[]
+    for key in range(len(myHashList)+1):
+        myList.append(key)
+    
+    return myList
+
+myHashList={(0,0):-1,(0,1):1,(1,0):1,(1,1):-1}
+#myFunctionPrint(myFunctionHashToList(myFunctionConvertToHash(myList)))
+myFunctionHashToList(myHashList)
